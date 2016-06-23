56ae4d928ec9d4b1c8b54cc069343e25d58c6f07
ulovebin <ulovewb@163.com>
2016-06-23 09:27:47 +0800

    Initial Commit

---
+++ WBLoading/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WBLoading
+//
+//  Created by tuhui－03 on 16/6/23.
+//  Copyright © 2016年 wb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
